2ff4daddf892d7842dbedecbbed6acf008d58858
boogie27 <anonyecharles@gmail.com>
2023-04-01 20:15:21 +0100

    payizzy-client

--- src/components/home/Faq.js
+++ src/components/home/Faq.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { 
-    faSortUp,
     faSortDown
  } from '@fortawesome/free-solid-svg-icons'
 
