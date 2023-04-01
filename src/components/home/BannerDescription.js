d47caf96e3a2aed15613373bcac2c5765639c35b
boogie27 <anonyecharles@gmail.com>
2023-04-01 21:36:35 +0100

    payizzy-client

--- src/components/home/BannerDescription.js
+++ src/components/home/BannerDescription.js
@@ -18,6 +18,26 @@ const BannerDescription = () => {
                             We are at you service with 24/7 support team.</p>
                     </div>
                 </div>
+                <div className="frame-item">
+                    <div className="title-header">
+                        <h3>Safe, fast and Uninterrupted Payment!</h3>
+                    </div>
+                    <div className="item-body">
+                        <p>
+                            We follow the developing technologies  and integrate them in accordance with every infrastructue. 
+                            We are at you service with 24/7 support team.</p>
+                    </div>
+                </div>
+                <div className="frame-item">
+                    <div className="title-header">
+                        <h3>Safe, fast and Uninterrupted Payment!</h3>
+                    </div>
+                    <div className="item-body">
+                        <p>
+                            We follow the developing technologies  and integrate them in accordance with every infrastructue. 
+                            We are at you service with 24/7 support team.</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
