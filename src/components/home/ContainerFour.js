9421b6e2e99f490871cb4c1a7f3403f2f1596404
boogie27 <anonyecharles@gmail.com>
2023-04-02 17:19:54 +0100

    payizzy-client

--- src/components/home/ContainerFour.js
+++ src/components/home/ContainerFour.js
@@ -22,11 +22,11 @@ const ContainerFour = () => {
         },
         tablet: {
           breakpoint: { max: 1024, min: 464 },
-          items: 4
+          items: 3
         },
         mobile: {
           breakpoint: { max: 464, min: 0 },
-          items: 3
+          items: 2
         }
       };
 
