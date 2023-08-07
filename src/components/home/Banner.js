d7a07c6997b62668f7256f68605f85e94327eeb0
boogie27 <anonyecharles@gmail.com>
2023-08-07 15:14:17 +0100

    payizzy-app

--- src/components/home/Banner.js
+++ src/components/home/Banner.js
@@ -60,18 +60,17 @@ const Description = ({bannerImage}) => {
     return (
         <div className="description">
             <div className="title-header">
-                <h3>Safe, fast and </h3>
-                <h3>Uninterrupted</h3>
-                <h3>Payment!</h3>
+                <h3>Safe, fast and Uninterrupted Payment!</h3>
             </div>
             <div claName="image">
                 <Image bannerImage={bannerImage}/>
             </div>
             <div className="body">
-                <p>We follow the developing technologies  and </p>
-                <p>integrate them in accordance with every </p>
-                <p>infrastructue. We are at you service with </p>
-                <p>24/7 support team.</p>
+                <p>We follow the developing technologies  and 
+                    integrate them in accordance with every 
+                    infrastructue. We are at you service with 
+                    24/7 support team. 
+                </p>
             </div>
         </div>
     )
